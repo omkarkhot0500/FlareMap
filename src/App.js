de36a60632375a65d54df96a0c242799623a3a75
omkarkhot0500 <omkarkhot0500@gmail.com>
2024-09-28 20:42:21 +0530

    FlareMap
    In search of fire

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Map from './components/Map'
+import Loader from './components/Loader'
+import Header from './components/Header'
+
+function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events');
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    }
+    fetchEvents();
+    console.log(eventData);
+  },[])
+
+
+
+  return (
+    <div>
+      <Header />
+      { !loading ? <Map eventData={eventData} /> : <Loader /> }
+    </div>
+  );
+}
+
+export default App;

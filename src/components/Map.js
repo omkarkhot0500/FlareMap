89ad4f54ef978b0585893527c8175cb3dabaca9d
omkarkhot0500 <omkarkhot0500@gmail.com>
2024-10-16 13:22:06 +0530

    chage in readme

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,42 +1,46 @@
-import { useState } from 'react'
-import GoogleMapReact from 'google-map-react'
-import LocationMarker from './LocationMarker'
-import LocationInfoBox from './LocationInfoBox'
+import { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
+import LocationInfoBox from "./LocationInfoBox";
 
 const Map = ({ eventData, center, zoom }) => {
-    const [locationInfo, setLocationInfo] = useState(null)
-    const markers = eventData.map(ev => {
-        if(ev.categories[0].id === 8) {
-            return <LocationMarker 
-            key={ev.id}
-            lat={ev.geometries[0].coordinates[1]}
-             lng={ev.geometries[0].coordinates[0]} 
-            onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
-        }
-        return null
-    })
+  const [locationInfo, setLocationInfo] = useState(null);
+  const markers = eventData.map((ev) => {
+    if (ev.categories[0].id === 8) {
+      return (
+        <LocationMarker
+          key={ev.id}
+          lat={ev.geometries[0].coordinates[1]}
+          lng={ev.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
+        />
+      );
+    }
+    return null;
+  });
 
-
-    return (
-        <div className="map">
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_SECRET_KEY }}
-                defaultCenter={ center }
-                defaultZoom={ zoom }
-            >
-                {markers}
-            </GoogleMapReact>
-            {locationInfo && <LocationInfoBox info={locationInfo} />}
-        </div>
-    )
-}
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_SECRET_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        {markers}
+      </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
+    </div>
+  );
+};
 
 Map.defaultProps = {
-    center: {
-        lat: 42.3265,
-        lng: -122.8756
-    },
-    zoom: 6
-}
+  center: {
+    // lat: 42.3265,
+    // lng: -122.8756
+    lat: 43.781666,
+    lng: -17.054833,
+  },
+  zoom: 1,
+};
 
-export default Map
+export default Map;
